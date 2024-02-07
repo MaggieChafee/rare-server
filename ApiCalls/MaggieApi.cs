24440d33ce37a069515fd33528514e9b5d010fd0
MaggieChafee <maggie.chafee@gmail.com>
2024-02-06 21:07:02 -0600

    first take on API calls

--- ApiCalls/MaggieApi.cs
+++ ApiCalls/MaggieApi.cs
@@ -1,9 +1,37 @@
-﻿namespace RareServer.ApiCalls
+﻿using RareServer.Models;
+
+namespace RareServer.ApiCalls
+
 {
     public static class MaggieApi
     {
         public static void Map(WebApplication app)
         {
+            // getAllTags
+            app.MapGet("/tags", () =>
+            {
+                return Results.Ok(TagsData.tags);
+            });
+            // getAllPostsTags
+            // updateTag
+            // createTag
+            app.MapPost("/tags", (Tag newTag) =>
+            {
+                newTag.Id = TagsData.tags.Count + 1;
+                TagsData.tags.Add(newTag);
+                return Results.Ok(newTag);
+                });
+            // deleteTag
+            app.MapDelete("/tags/{id}", (int id) =>
+            {
+                Tag deleteTag = TagsData.tags.FirstOrDefault(t => t.Id == id);
+                if (deleteTag == null)
+                {
+                    return Results.NotFound();
+                } 
+                TagsData.tags.Remove(deleteTag);
+                return Results.Ok();
+            });
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
 using RareServer.ApiCalls;
+using RareServer.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+
+List<Tag> tags = new List<Tag>
+{
+    new Tag { Id = 1, Label = "The Latest" },
+    new Tag { Id = 2, Label = "News"},
+    new Tag { Id = 3, Label = "Books & Culture"},
+    new Tag { Id = 4, Label = "Fiction & Poetry"},
+    new Tag { Id = 5, Label = "Humor & Cartoons"}
+};
 
 // Add services to the container.
 
@@ -23,5 +33,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+MaggieApi.Map(app);
 
 app.Run();
