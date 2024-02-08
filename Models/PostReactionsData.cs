a9160e488345d1bf3304fe0ab4f7429f7a91f189
joeyebach <connect@joeyebach.com>
2024-02-07 20:29:36 -0600

    merging group code

---
+++ Models/PostReactionsData.cs
@@ -0,0 +1,45 @@
+﻿namespace RareServer.Models
+{
+	public class PostReactionsData
+	{
+		public static List<PostReactions> postReactions = new()
+		{
+			new PostReactions
+			{
+				Id = 1,
+				ReactionId = 1,
+				UserId = 1,
+				PostId = 1,
+			},
+            new PostReactions
+            {
+                Id = 2,
+                ReactionId = 2,
+                UserId = 2,
+                PostId = 2,
+            },
+            new PostReactions
+            {
+                Id = 3,
+                ReactionId = 3,
+                UserId = 3,
+                PostId = 3,
+            },
+            new PostReactions
+            {
+                Id = 4,
+                ReactionId = 4,
+                UserId = 4,
+                PostId = 4,
+            },
+               new PostReactions
+            {
+                Id = 4,
+                ReactionId = 4,
+                UserId = 4,
+                PostId = 2,
+            },
+        };
+	}
+}
+
---
+++ Models/ReactionsData.cs
@@ -0,0 +1,29 @@
+﻿namespace RareServer.Models
+{
+    public class ReactionsData
+    {
+        public static List<Reactions> reactions = new()
+        {
+            new Reactions
+            {
+                Id = 1,
+                Emoji = "Happy",
+            },
+            new Reactions
+            {
+                Id = 2,
+                Emoji = "Sad",
+            },
+            new Reactions
+            {
+                Id = 3,
+                Emoji = "Silly",
+            },
+            new Reactions
+            {
+                Id = 4,
+                Emoji = "Mad",
+            },
+        };
+    }
+}
